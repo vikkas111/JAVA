107c8866b6c8d356f91729969856e3bb0dfdc2f8
vikkas111 <vikaskoduri@gmail.com>
2018-05-03 11:25:13 -0500

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,27 @@
+package com.order;
+
+public class Order {
+int orderno;
+int productid;
+int quantity;
+public int getOrderno() {
+	return orderno;
+}
+public void setOrderno(int orderno) {
+	this.orderno = orderno;
+}
+public int getProductid() {
+	return productid;
+}
+public void setProductid(int productid) {
+	this.productid = productid;
+}
+public int getQuantity() {
+	return quantity;
+}
+public void setQuantity(int quantity) {
+	this.quantity = quantity;
+}
+
+
+}
